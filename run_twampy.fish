f2de655d591568c267506f164f0ae771995d7f9c
lcj005 <lcj005@linux.com>
2024-04-30 09:32:06 +0100

    modified:   run_twampy.fish

--- run_twampy.fish
+++ run_twampy.fish
@@ -1,13 +1,27 @@
 #!/usr/bin/env fish
 
+# Check if sufficient arguments are provided
+if test (count $argv) -lt 2
+    echo "Usage: sudo ./run_twampy.fish [IP Address] [Port]"
+    exit 1
+end
+
+# Assign arguments to variables
+set ip_address $argv[1]
+set port $argv[2]
+
+# Display the variables (for debugging purposes)
+echo "Using IP Address: $ip_address"
+echo "Using Port: $port"
+
 # Disable the firewall
 echo "Disabling the firewall..."
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate off
 echo "Firewall is disabled."
 
-# Run the Python script
+# Run the Python script with the provided IP address and port
 echo "Running the Python script..."
-python twampy.py sender 192.168.191.223:27031
+python twampy.py sender $ip_address:$port
 
 # Re-enable the firewall
 echo "Re-enabling the firewall..."
